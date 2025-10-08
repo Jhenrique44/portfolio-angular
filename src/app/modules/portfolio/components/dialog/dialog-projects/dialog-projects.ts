de79cd957782daafad8aa1b0fa5310e03db21b47
Jhenrique44 <jo-henrique8@hotmail.com>
2025-10-08 15:56:38 -0300

    feat: add dialog projects component and integrate with projects component

---
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-projects',
+  imports: [],
+  templateUrl: './dialog-projects.html',
+  styleUrl: './dialog-projects.scss'
+})
+export class DialogProjectsComponent {
+
+}
--- src/app/modules/portfolio/components/projects/projects.ts
+++ src/app/modules/portfolio/components/projects/projects.ts
@@ -1,13 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.components';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
       src: '../../../../../assets/images/projects',
@@ -21,5 +25,71 @@ export class ProjectsComponent {
         },
       ],
     },
+    {
+      src: '../../../../../assets/images/projects',
+      alt: '',
+      title: '',
+      description: '',
+      links: [
+        {
+          name: '',
+          url: '',
+        },
+      ],
+    },
+    {
+      src: '../../../../../assets/images/projects',
+      alt: '',
+      title: '',
+      description: '',
+      links: [
+        {
+          name: '',
+          url: '',
+        },
+      ],
+    },
+    {
+      src: '../../../../../assets/images/projects',
+      alt: '',
+      title: '',
+      description: '',
+      links: [
+        {
+          name: '',
+          url: '',
+        },
+      ],
+    },
+    {
+      src: '../../../../../assets/images/projects',
+      alt: '',
+      title: '',
+      description: '',
+      links: [
+        {
+          name: '',
+          url: '',
+        },
+      ],
+    },
+    {
+      src: '../../../../../assets/images/projects',
+      alt: '',
+      title: '',
+      description: '',
+      links: [
+        {
+          name: '',
+          url: '',
+        },
+      ],
+    },
   ]);
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, { 
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    })
+  }
 }
