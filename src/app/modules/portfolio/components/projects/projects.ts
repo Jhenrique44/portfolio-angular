c644c3629e90846d520bda24756648b4d9f71749
Jhenrique44 <jo-henrique8@hotmail.com>
2025-10-08 16:47:20 -0300

    feat: implement dialog projects component with close functionality and dynamic content feat: add SVG icons for close, mail, and WhatsApp style: update header styles and adjust image dimensions fix: correct project dialog panel class name in enum style: modify theme variables for primary colors

--- src/app/modules/portfolio/components/projects/projects.ts
+++ src/app/modules/portfolio/components/projects/projects.ts
@@ -16,12 +16,12 @@ export class ProjectsComponent {
     {
       src: '../../../../../assets/images/projects',
       alt: '',
-      title: '',
-      description: '',
+      title: 'Resort',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       links: [
         {
-          name: '',
-          url: '',
+          name: 'Resort',
+          url: 'https://www.linkedin.com/in/jhenriquefq',
         },
       ],
     },
